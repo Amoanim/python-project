7c7ef2449197042ff2d5991d51af9c9a37c4c6ac
Amoanim <theophilusakwasiamoanimduah@gmail.com>
2025-06-10 16:31:09 -0700

    update: function

---
+++ pizza/main.py
@@ -0,0 +1,81 @@
+# pizza sizes and prices
+small = 100.00
+medium = 125.00
+large = 150.00
+family = 200.00
+
+# sides and prices
+small_pepperoni = 20
+medium_large = 40
+family_pepperoni = 50
+cheese = 20
+
+# total bill 
+total_bill = 0
+
+def small_pizza():
+        total_bill = small
+        add_pepperoni = input("Would you add extra pepperoni?[y/n]\n").lower()
+        if add_pepperoni == 'y':
+            total_bill += small_pepperoni
+        extra_cheese = input("Would you add and extra cheese?[y/n]\n").lower()
+        if extra_cheese == 'y':
+            total_bill += cheese
+        print(f"\nTotal bill Ghc{total_bill}\n")
+
+def medium_pizza():
+        total_bill = medium
+        add_pepperoni = input("Would you add extra pepperoni?[y/n]\n").lower()
+        if add_pepperoni == 'y':
+            total_bill += medium_large
+        extra_cheese = input("Would you add and extra cheese?[y/n]\n").lower()
+        if extra_cheese == 'y':
+            total_bill += cheese
+        print(f"\nTotal bill Ghc{total_bill}\n")
+
+def large_pizza():
+        total_bill = large
+        add_pepperoni = input("Would you add extra pepperoni?[y/n]\n").lower()
+        if add_pepperoni == 'y':
+            total_bill += medium_large
+        extra_cheese = input("Would you add and extra cheese?[y/n]\n").lower()
+        if extra_cheese == 'y':
+            total_bill += cheese
+        print(f"\nTotal bill Ghc{total_bill}\n")
+
+def family_pizza():
+        total_bill = family
+        add_pepperoni = input("Would you add extra pepperoni?[y/n]\n").lower()
+        if add_pepperoni == 'y':
+            total_bill += family_pepperoni
+        extra_cheese = input("Would you add and extra cheese?[y/n]\n").lower()
+        if extra_cheese == 'y':
+            total_bill += cheese
+        print(f"\nTotal bill Ghc{total_bill}\n")
+# infinite loop
+while True:
+    print("**Welcome To Pizza Palace**")
+    
+    try:
+        menu_choice = input("s for small\n" 
+                    "m for medium\n"
+                    "l for large\n" 
+                    "f for family\n"
+                    "e to exit the program\n"
+                    "Please select your choice?: ").lower()
+
+        if menu_choice == 's':
+            small_pizza()
+        elif menu_choice == 'm':
+            medium_pizza()
+        elif menu_choice == 'l':
+            large_pizza()
+        elif menu_choice == 'f':
+            family_pizza()
+        elif menu_choice == "e":
+            print("\n**Closing Program**\n")
+            exit()
+        else:
+            print("\nKindly provide an option from the manu!!!.\n")
+    except ValueError:
+        print("\nKindly enter a valid value\n")
